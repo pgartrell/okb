01e14fdee24bc3ad8c31d846fa6c9e98cfc53915
Sheila Strahan <smstrahan@gmail.com>
2022-01-29 16:57:25 -0700

    🤪faces and join added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeAccordion from './components/HomeAccordion';
+import HomeJoinLink from './components/HomeJoinLink';
 
 function App() {
   return (
     <div className="App">
       <HomeAccordion />
+      <HomeJoinLink />
     </div>
   );
 }
