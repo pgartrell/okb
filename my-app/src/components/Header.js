8017253b443cd778ba1514b2b7c62b86c678b6f3
Raul Escobedo <rjescobedo@gmail.com>
2022-01-30 12:12:57 -0600

    Added Navbar

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { Container, Navbar, Nav, } from "react-bootstrap";
+import { 
+  Container,
+  Navbar, 
+  Nav,
+  NavbarBrand 
+} from "react-bootstrap";
 import {NavLink} from "react-router-dom"
+import LogoImage from '../images/okb-logo.png';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" expand="lg">
-      <Container>       
+    <Navbar bg="light" variant="light" sticky="top" expand="lg">
+      <Container fluid>  
+        <NavbarBrand href="#">
+            <img
+              src={LogoImage}
+            />
+        </NavbarBrand>     
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="navBarLinks">
-            <NavLink to="/landingPage">Landing</NavLink>
-            <NavLink to="/mobileClinicPage">Mobile Clinic</NavLink>
-            <NavLink to="/onlineTherapyPage">Online Therapy</NavLink>
-            <NavLink to="/resourcesPage">Resources</NavLink>
+            <NavLink className="items-navigation" to="/landingPage">Landing</NavLink>
+            <NavLink className="items-navigation" to="/mobileClinicPage">Mobile Clinic</NavLink>
+            <NavLink className="items-navigation" to="/onlineTherapyPage">Online Therapy</NavLink>
+            <NavLink className="items-navigation" to="/resourcesPage">Resources</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
