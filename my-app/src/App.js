4ba58801572c88cdbf03c98e16456faaa0fb5e89
pgartrell <preciousrg@outlook.com>
2022-01-29 20:18:47 -0500

    routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import MobileClinicPage from "./pages/MobileClinicPage";
+import OnlineTherapyPage from "./pages/OnlineTherapyPage";
+import ResourcesPage from "./pages/ResourcesPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route>
+            <Route path="/landingPage" element={<LandingPage />} />
+            <Route path="/mobileClinicPage" element={<MobileClinicPage />} />
+
+            <Route path="/onlineTherapyPage" element={<OnlineTherapyPage />} />
+
+            <Route path="/resourcesPage" element={<ResourcesPage />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
