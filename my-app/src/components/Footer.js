5e43c853f35b20484aaf74d3e700b6ca469d6261
Raul Escobedo <rjescobedo@gmail.com>
2022-01-30 04:54:31 -0600

    Added Font-Awesome NPM

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+                <ul>
+                    <h3>Menu</h3>
+                    <li>Who We Are</li>
+                    <li>What We Do</li>
+                    <li>Our Impact</li>
+                    <li>Work With Us</li>
+                    <li>Donate</li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
