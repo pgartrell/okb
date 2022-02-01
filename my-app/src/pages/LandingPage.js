1266fb81a9f050cf01de1232be8e633d3a4c1f88
Raul Escobedo <rjescobedo@gmail.com>
2022-02-01 10:09:59 -0600

    Home Resources

--- my-app/src/pages/LandingPage.js
+++ my-app/src/pages/LandingPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import HomeHeroSection from "../components/HomeHeroSection";
 import HomeSummaries from "../components/HomeSummaries";
+import HomeResources from "../components/HomeResources";
+import HomeAccordion from '../components/HomeAccordion';
+import HomeJoinLink from '../components/HomeJoinLink';
 
 const LandingPage = () => {
   return (
         <div>
             <HomeHeroSection />
             <HomeSummaries />
+            <HomeAccordion />
+            <HomeJoinLink />
+            <HomeResources />
         </div>
   )
 };
