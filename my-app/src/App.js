c98d10208ff7a2973a5e38e606ddd5675311607b
Sheilabob <87608520+Sheilabob@users.noreply.github.com>
2022-01-31 17:43:49 -0700

    Revert "Footer"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ import MobileClinicPage from "./pages/MobileClinicPage";
 import OnlineTherapyPage from "./pages/OnlineTherapyPage";
 import ResourcesPage from "./pages/ResourcesPage";
 import Header from "./components/Header";
-import Footer from './components/Footer'
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeAccordion from './components/HomeAccordion';
@@ -28,14 +28,17 @@ function App() {
           <Route>
             <Route path="/landingPage" element={<LandingPage />} />
             <Route path="/mobileClinicPage" element={<MobileClinicPage />} />
+
             <Route path="/onlineTherapyPage" element={<OnlineTherapyPage />} />
+
             <Route path="/resourcesPage" element={<ResourcesPage />} />
           </Route>
         </Routes>
       </Router>
+
       <HomeAccordion />
+
       <HomeJoinLink />
-      <Footer />
 
 
     </div>
