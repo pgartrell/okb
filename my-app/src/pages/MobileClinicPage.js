0c2dc2ec31ea52ccdb89a6723ab0748053e0cf60
Sheila Strahan <smstrahan@gmail.com>
2022-02-03 20:08:12 -0700

    mobile clinic accordion added

--- my-app/src/pages/MobileClinicPage.js
+++ my-app/src/pages/MobileClinicPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import MobileAccordion from '../components/MobileFAQAccordion';
 
 const MobileClinicPage = () => {
   return (
     <div>
       <h1>Mobile Clinic Page</h1>
+      <MobileAccordion />
     </div>
   );
 };
