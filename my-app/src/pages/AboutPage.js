85eb6338f792d82dd91de53e32f50e9cc00f2e9c
Raul Escobedo <rjescobedo@gmail.com>
2022-02-20 07:00:18 -0600

    About Founders Story Video/Section

--- my-app/src/pages/AboutPage.js
+++ my-app/src/pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutHero from '../components/AboutHero';
+import AboutFoundersStory from '../components/AboutFoundersStory';
 
 export default function AboutPage() {
     return (
         <div>
             <AboutHero />
+            <AboutFoundersStory />
         </div>
     );
 }
