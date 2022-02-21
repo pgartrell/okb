f68147fc57a1e30a36902314f3fef05adb48df3d
Raul Escobedo <rjescobedo@gmail.com>
2022-02-21 17:38:37 -0600

    About Our Vision

---
+++ my-app/src/components/AboutOurVision.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import Ghana from '../images/ghana-country.png';
+
+export default function AboutOurVision() {
+    return (
+        <Container className="about-vision-container">
+            <Row>
+                <Col>
+                    <img src={Ghana} alt="Ghana" className="about-vision-image"/>
+                    <h2 className="about-vision-title">Our Vision for Ghana</h2>
+                    <p className="about-vision-text">To ensure that equitable and quality healthcare is accessible to all Ghanaians</p>
+                    <button className="btn-okb-primary">See How We Do It</button>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
