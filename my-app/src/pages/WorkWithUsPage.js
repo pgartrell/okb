e1b286003bd81102731815ec52b13225003a7e7c
Sheila Strahan <smstrahan@gmail.com>
2022-02-22 19:10:45 -0700

    volunteer in ghana cards

--- my-app/src/pages/WorkWithUsPage.js
+++ my-app/src/pages/WorkWithUsPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WorkWithUsHero from "../components/WorkWithUsHero";
+import VolunteerInGhana from "../components/VolunteerInGhana";
 
 const WorkWithUsPage = () => {
   return (
     <div>
       <WorkWithUsHero />
+      <VolunteerInGhana />
     </div>
   );
 };
