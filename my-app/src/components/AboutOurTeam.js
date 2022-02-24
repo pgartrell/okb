2c22e98f283bd6af5c4873c9e0ede11eb6dbca4d
Raul Escobedo <rjescobedo@gmail.com>
2022-02-23 20:12:38 -0600

    About Our Team & Be Part of the Change Merge

--- my-app/src/components/AboutOurTeam.js
+++ my-app/src/components/AboutOurTeam.js
@@ -5,10 +5,16 @@ export default function AboutOurTeam() {
     return (
             <Container className="about-team-container" fluid>
                 <Row>
-                    <Col xs={12}>
+                    <Col xs={12} md={6} className="mb-5">
                         <h1 className="about-team-title">Our Wonderful Team</h1>
                         <p className="about-team-text">Our Medical Staff, Students, Volunteers, Health workers, Administrators, Board Members and everyone else that impacts our organization, Thank you!</p>
                         <button className="btn-okb-clear">Meet The Team</button>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <h1 className="about-team-title">Be a part of the change</h1>
+                        <p className="about-team-text">You can make a difference by supporting our mission for a better Ghana and a better world.</p>
+                        <button className="btn-okb-primary">Donate</button>
+                        <button className="btn-okb-secondary">Volunteer</button>
                     </Col>
                 </Row>
             </Container>
