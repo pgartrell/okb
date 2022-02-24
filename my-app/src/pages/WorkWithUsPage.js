45f700a4be54cae1264e809fa9331b05bbbd1933
Sheila Strahan <smstrahan@gmail.com>
2022-02-23 19:03:38 -0700

    volunteer testimonials

--- my-app/src/pages/WorkWithUsPage.js
+++ my-app/src/pages/WorkWithUsPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import WorkWithUsHero from "../components/WorkWithUsHero";
 import VolunteerInGhana from "../components/VolunteerInGhana";
+import VolunteerTestimonials from "../components/VolunteerTestimonials";
 
 const WorkWithUsPage = () => {
   return (
     <div>
       <WorkWithUsHero />
       <VolunteerInGhana />
+      <VolunteerTestimonials />
     </div>
   );
 };
