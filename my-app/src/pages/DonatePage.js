5ad1299f5433056e90ab404105e0fcd2df408c69
Sheila Strahan <smstrahan@gmail.com>
2022-02-28 18:33:09 -0700

    hero section for donate page

---
+++ my-app/src/pages/DonatePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DonateHero from "../components/DonateHero";
+
+const DonatePage = () => {
+  return (
+    <div>
+      <DonateHero />
+    </div>
+  );
+};
+
+export default DonatePage;
