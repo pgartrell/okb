5666ffe500dfd429f74e976e4a5f570c166c36df
Sheila Strahan <smstrahan@gmail.com>
2022-03-02 17:20:05 -0700

    started work on donation form 1

--- my-app/src/pages/DonatePage.js
+++ my-app/src/pages/DonatePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DonateHero from "../components/DonateHero";
+import DonateForm from "../components/DonateForm";
 
 const DonatePage = () => {
   return (
     <div>
       <DonateHero />
+      <DonateForm />
     </div>
   );
 };
