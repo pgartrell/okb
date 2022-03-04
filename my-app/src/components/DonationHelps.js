fe3c63a066f1752d4395ab2f11409b129cc45412
Raul Escobedo <rjescobedo@gmail.com>
2022-03-04 10:18:05 -0600

    Final Changes to Donation Helps Section

--- my-app/src/components/DonationHelps.js
+++ my-app/src/components/DonationHelps.js
@@ -3,23 +3,23 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export default function DonationHelps() {
     return (
-        <Container className="donation-helps-container">
+        <Container className="donation-helps-container" fluid>
             <Row>
-                <Col xs={12} md={6}>
+                <Col md={6}>
                     <h2 className="donation-helps-title">How Your Donation Helps</h2>
                 </Col>
-                <Col xs={12} md={6}>
+                <Col md={6}>
                     <ul className="donation-helps-list">
-                        <li>It brings medical supplies for health screenings.</li>
-                        <li>It empowers patients throughout Ghana.</li>
-                        <li>It initiates projects that better the lives of communities.</li>
-                        <li>It provides health education to the people of Ghana.</li>
+                        <li className="donation-helps-list-item">It brings medical supplies for health screenings.</li>
+                        <li className="donation-helps-list-item">It empowers patients throughout Ghana.</li>
+                        <li className="donation-helps-list-item">It initiates projects that better the lives of communities.</li>
+                        <li className="donation-helps-list-item">It provides health education to the people of Ghana.</li>
                     </ul>
                 </Col>
             </Row>
             <Row>
-                <Col xs={12}>
-                    <p className="donation-helps-text">OKB Hope Foundation is a 501(c)3 not-for profit organization, this means that we rely on donations from people like you to achieve our mission of making quality healthcare accessible throughout Ghana.</p>
+                <Col>
+                    <p className="text-muted donation-helps-text">OKB Hope Foundation is a 501(c)3 not-for profit organization, this means that we rely on donations from people like you to achieve our mission of making quality healthcare accessible throughout Ghana.</p>
                 </Col>
             </Row>
         </Container>
