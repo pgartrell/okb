fde23a029ff1185222d5f8686161c25191275d95
pgartrell <preciousrg@outlook.com>
2022-03-13 08:52:23 -0400

    HomeWWD

--- my-app/src/pages/LandingPage.js
+++ my-app/src/pages/LandingPage.js
@@ -2,19 +2,21 @@ import React from "react";
 import HomeHeroSection from "../components/HomeHeroSection";
 import HomeSummaries from "../components/HomeSummaries";
 import HomeResources from "../components/HomeResources";
-import HomeAccordion from '../components/HomeAccordion';
-import HomeJoinLink from '../components/HomeJoinLink';
+import HomeAccordion from "../components/HomeAccordion";
+import HomeJoinLink from "../components/HomeJoinLink";
+import HomeWWD from "../components/HomeWWD";
 
 const LandingPage = () => {
   return (
-        <div>
-            <HomeHeroSection />
-            <HomeSummaries />
-            <HomeAccordion />
-            <HomeJoinLink />
-            <HomeResources />
-        </div>
-  )
+    <div>
+      <HomeHeroSection />
+      <HomeSummaries />
+      <HomeWWD />
+      <HomeAccordion />
+      <HomeJoinLink />
+      <HomeResources />
+    </div>
+  );
 };
 
 export default LandingPage;
