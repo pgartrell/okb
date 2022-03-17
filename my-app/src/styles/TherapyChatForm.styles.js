1634a239bbc7a60724f21434568a7bf965d7ff0e
pgartrell <preciousrg@outlook.com>
2022-03-17 19:15:38 -0400

    Form almost complete!

--- my-app/src/styles/TherapyChatForm.styles.js
+++ my-app/src/styles/TherapyChatForm.styles.js
@@ -2,15 +2,17 @@ import styled from "styled-components";
 
 export const Labels = styled.label `
     padding: 5px;
+    font-size: 20px ;
 `
 export const FormP = styled.p `
     padding-top: 5px;
     text-align: left ;
+    font-size: 25px;
 `
-export const LabelsRating = styled.label `
-    /* display: block; */
-`
+
 
 export const FormInputs = styled.input `
     text-align: left ;
 `
+
+
