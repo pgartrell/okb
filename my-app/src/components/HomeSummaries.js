5cae056dad21b2e1cf712c02b4fb7f54e4af1eb2
Raul Escobedo <rjescobedo@gmail.com>
2022-03-19 06:22:05 -0500

    Home Summaries Edits

--- my-app/src/components/HomeSummaries.js
+++ my-app/src/components/HomeSummaries.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 import testDoctorImage from "../images/testDoctorImage.jpg";
 import "../styles/HomeSummaries.css"
 import {Link} from "react-router-dom"
@@ -6,7 +7,7 @@ import {Link} from "react-router-dom"
 
 
 export default function HomeSummaries() {
-  const [summaries, setSummaries] = useState([
+  const [summaries] = useState([
     {
       title: "Mobile Clinic",
       text: "This will explain the mobile clinic",
@@ -26,18 +27,23 @@ export default function HomeSummaries() {
 
   const cards = summaries.map((item) => {
     return (
-      
-        <div className="homeSummariesContainer">
-          <div className="homeSummariesImage">
-            <img src={item.img} />
-          </div>
-          <div className="homeSummariesText">
-            <h1 className="homeSummariesTitle">{item.title}</h1>
-            <p className="homeSummariesParagraph">{item.text}</p>
-            <Link to={item.href}><button className="btn-okb-primary">Get Started</button></Link>
-            <Link to={item.href}><button className="btn-okb-secondary">Returning Patients</button></Link>
-          </div>
-        </div>      
+        <Container className="homeSummariesContainer">
+          <Row>
+            <Col xs={12} lg={6}>
+                <div className="homeSummariesImage">
+                    <img src={item.img} />
+                </div>
+            </Col>
+            <Col xs={12} lg={6}>
+                <div className="homeSummariesText">
+                    <h1 className="homeSummariesTitle">{item.title}</h1>
+                    <p className="homeSummariesParagraph">{item.text}</p>
+                    <Link to={item.href}><button className="btn-okb-primary">Get Started</button></Link>
+                    <Link to={item.href}><button className="btn-okb-secondary">Returning Patients</button></Link>
+                </div>
+            </Col>
+          </Row>
+        </Container>      
     );
   });
 
