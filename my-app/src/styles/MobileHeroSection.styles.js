d35d0e7eebc1dd18c0067c87477ed5459c5688cd
Raul Escobedo <rjescobedo@gmail.com>
2022-03-24 05:43:35 -0500

    Mobile Hero Section Edits

--- my-app/src/styles/MobileHeroSection.styles.js
+++ my-app/src/styles/MobileHeroSection.styles.js
@@ -9,10 +9,41 @@ export const MobileJumbotronContainer = styled.section`
   height: 70vh;
   width: 100vw;
   background-size: cover;
-  background-position: center center; /* or 50% 50% */
+  background-position: 65%; /* or 50% 50% */
   background-repeat: no-repeat;  
   background-image: url(${testImage2});  
+`
+
+export const Jumbotron = styled.div`
+  /* padding-top: 100px;
+  padding-bottom: 100px;
+  margin: 50px 50px; */
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+  color: whitesmoke;
+  justify-content: flex-start;
   
+`;
+
+export const MobileHeroTitle = styled.h1`
+  font-size: 5.5rem;
+  text-shadow: .5px .5px black;
+  font-weight: normal;
+  text-align: left;
+  padding-left: 50px;
+  display: flex;
+
+`;
+
+export const MobileHeroSubtitle = styled.p `
+    font-size: 2rem;
+    text-shadow: .5px .5px black;
+    font-weight: 600;
+    text-align: left;
+    padding-left: 50px;
 `
 
 //No longer in use. Use for reference
