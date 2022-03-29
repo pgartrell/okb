a93a2318751df285d06453c00078ccd20f046a2f
pgartrell <preciousrg@outlook.com>
2022-03-29 16:26:09 -0400

    FAQ Pages

--- my-app/src/styles/VAQuestions.styles.js
+++ my-app/src/styles/VAQuestions.styles.js
@@ -18,3 +18,34 @@ export const VAQuestionsButton = styled.button`
   font-size: 24px;
   white-space: nowrap ;
 `;
+
+/*VAFAQ Accordian */
+export const VAFAQh2 = styled.h2 `
+  padding-left: 200px ;
+  @media (max-width: 1000px) {
+    padding-left: 50px;
+  }
+`
+
+/*VA Need Help Section */
+export const VANeedHelpdiv = styled.div`
+  padding: 100px ;
+  height: 400px; 
+  background-color: white;
+  background-image: linear-gradient(180deg, white, #efeff5, #d0d0e2);
+  @media (max-width: 400px) {
+    padding-left: 60px;
+  }
+
+`
+
+export const VANeedHelph2 = styled.h1`
+  display: block;
+  font-size: 2em;
+  font-weight:bolder ;
+  white-space: nowrap ;
+`
+
+export const VANeedHelph4 = styled.h4`
+  font-size: 20px ;
+`
